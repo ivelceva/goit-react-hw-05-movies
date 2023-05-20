dde1793278abc617f78534ba17fb0221df1cbdb6
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-05-20 16:03:11 -0700

    api_setup

---
+++ src/Fetch.jsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const KEY = 'b889b1f15800992b1379ee4939d9484c';
+
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+import { getTrendingMovies } from '../../Fetch.jsx';
+import { Link, useLocation } from 'react-router-dom';
+import css from './Home.module.css';
+
+
