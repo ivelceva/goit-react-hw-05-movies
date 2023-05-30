8d63127ca48917a8c131c0e71cc7a7a1a0a40b63
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-05-30 20:18:06 +0000

    Deploying to gh-pages from @ ivelceva/goit-react-hw-05-movies@4d13a57c8fb02602127503f9b99029c73c056c7e 🚀

--- static/js/758.8894e992.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[758],{758:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("h2",{children:"MovieDetails"})}}}]);
-//# sourceMappingURL=758.8894e992.chunk.js.map
