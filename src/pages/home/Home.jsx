6842e2803bff17d2b15eb1fd0aaf7c15263e60ec
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-05-30 14:47:49 -0700

    gallery_style

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -46,7 +46,6 @@ export default function Home() {
                   width=""
                   height=""
                 />
-                <p>{title}</p>
               </Link>
             </li>
           ))}
