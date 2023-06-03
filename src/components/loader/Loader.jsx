fada584cea129113544c221e96f499969bf3e077
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-03 13:18:07 -0700

    movieDetails_add

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './Loader.module.css';
+
+export default function Loader() {
+  return (
+    <div className={css.backdrop}>
+      <div className={css.loader}></div>
+    </div>
+  );
+}
--- src/pages/movieDetails/MovieDetails.jsx
+++ src/pages/movieDetails/MovieDetails.jsx
@@ -1,9 +1,105 @@
+import { Outlet, useParams, useNavigate, Link, useLocation } from 'react-router-dom';
+import { useState, useEffect, Suspense } from 'react';
+import { getMovieDetails } from '../../Fetch.jsx';
+import Loader from 'components/loader/Loader';
+import noImg from '..//..//images/No_image.jpg';
 import css from './MovieDetails.module.css';
 
+
 export default function MovieDetails() {
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { movieId } = useParams();
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  const from = location.state?.from || '/';
+
+  useEffect(() => {
+    const movieDetails = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const result = await getMovieDetails(movieId);
+        setMovie(result);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    movieDetails();
+  }, [movieId]);
+
+  const goBack = () => navigate(-1);
+  const isCastDetails = location.pathname.includes('cast');
+  const castLink = isCastDetails
+    ? `/movies/${movieId}`
+    : `/movies/${movieId}/cast`;
+  const isReviewDetails = location.pathname.includes('reviews');
+  const reviewLink = isReviewDetails
+    ? `/movies/${movieId}`
+    : `/movies/${movieId}/reviews`;
+
   return (
-    <>
-      <h2 className={css.detailsTitle}>Movie Details</h2>;
-    </>
+    <div className={css.container}>
+      <button className={css.button} onClick={goBack}>
+        Go back
+      </button>
+      {loading && <Loader />}
+      {error && <p>Something went wrong</p>}
+      {movie && (
+        <div className={css.movieDetails}>
+          <div className={css.boxImg}>
+            <img
+              className={css.img}
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                  : noImg
+              }
+              alt="poster"
+              width=""
+              height=""
+            />
+          </div>
+          <div className={css.boxInfo}>
+            <h2>{movie.title}</h2>
+            <p>User Score: {`${(movie.vote_average * 10).toFixed(1)}`}%</p>
+            <h3>Overview</h3>
+            <p>{`${movie.overview}`}</p>
+            <h3>Genres</h3>
+            <p>{`${movie.genres.map(genre => genre.name).join(' / ')}`}</p>
+            <div className={css.boxAddInfo}>
+              <h2>Additional information</h2>
+              <Link
+                className={css.castLink}
+                state={{ from }}
+                to={castLink}
+                replace
+              >
+                Cast
+              </Link>
+              <Link
+                className={css.reviewLink}
+                state={{ from }}
+                to={reviewLink}
+                replace
+              >
+                Reviews
+              </Link>
+              {/* <Cast />
+              <Reviews /> */}
+            </div>
+          </div>
+        </div>
+      )}
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
   );
 }
