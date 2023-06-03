6cd9444a9b4f158635920f048f491da6daf70a56
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-03 16:33:24 -0700

    header_fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,15 @@ export default function Header() {
     <>
       <div className={css.container}>
         <nav className={css.header}>
-          <Link className={css.navBtn} to="/">Home</Link>
-          <Link className={css.navBtn} to="/movies">Movies</Link>
+          <h1 className={css.title}>Blockbuster</h1>
+          <div className={css.btnWrap}>
+            <Link className={css.navBtn} to="/">
+              Home
+            </Link>
+            <Link className={css.navBtn} to="/movies">
+              Movies
+            </Link>
+          </div>
         </nav>
       </div>
       <Outlet />
