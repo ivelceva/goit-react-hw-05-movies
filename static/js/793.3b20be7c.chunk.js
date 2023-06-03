ce09b1bd9fb4a6313dd19bb7a9e928f01fdd5ba0
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-03 20:18:51 +0000

    Deploying to gh-pages from @ ivelceva/goit-react-hw-05-movies@fada584cea129113544c221e96f499969bf3e077 🚀

--- static/js/793.3b20be7c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[793],{793:function(e,t,a){a.r(t),a.d(t,{default:function(){return n}});var r="MovieDetails_detailsTitle__MO9iu",i=a(184);function n(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h2",{className:r,children:"Movie Details"}),";"]})}}}]);
-//# sourceMappingURL=793.3b20be7c.chunk.js.map
