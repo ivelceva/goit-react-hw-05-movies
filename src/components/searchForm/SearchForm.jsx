a7229d369cfc3b43c51e7b48959ea494f307404e
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-07 09:15:04 -0700

    SearchForm_fix

--- src/components/searchForm/SearchForm.jsx
+++ src/components/searchForm/SearchForm.jsx
@@ -6,8 +6,13 @@ export default function SearchForm({
   searchMovie,
   setSearchMovie,
 }) {
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    handleSubmit();
+  };
+
   return (
-    <form className={css.searchForm} onSubmit={handleSubmit}>
+    <form className={css.searchForm} onSubmit={handleFormSubmit}>
       <input
         onChange={e => setSearchMovie(e.target.value)}
         type="text"
@@ -23,3 +28,29 @@ export default function SearchForm({
     </form>
   );
 }
+
+// import React from 'react';
+// import css from './SearchForm.module.css';
+
+// export default function SearchForm({
+//   handleSubmit,
+//   searchMovie,
+//   setSearchMovie,
+// }) {
+//   return (
+//     <form className={css.searchForm} onSubmit={handleSubmit}>
+//       <input
+//         onChange={e => setSearchMovie(e.target.value)}
+//         type="text"
+//         autoComplete="off"
+//         autoFocus
+//         placeholder="Search movie"
+//         value={searchMovie}
+//         className={css.searchInput}
+//       />
+//       <button className={css.searchButton} type="submit">
+//         &#128269;
+//       </button>
+//     </form>
+//   );
+// }
--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -25,9 +25,7 @@ export default function Movies() {
     }
   }, [searchParams]);
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-
+  const handleSubmit = async () => {
     if (searchMovie.trim()) {
       const { results } = await searchMovies(searchMovie);
 
@@ -55,6 +53,64 @@ export default function Movies() {
     </>
   );
 }
+
+// import React, { useState, useEffect } from 'react';
+// import { useSearchParams, useNavigate } from 'react-router-dom';
+// import { searchMovies } from '../../Fetch.jsx';
+// import MovieList from 'components/movieList/MovieList.jsx';
+// import SearchForm from 'components/searchForm/SearchForm.jsx';
+
+// export default function Movies() {
+//   const [searchMovie, setSearchMovie] = useState('');
+//   const [movies, setMovies] = useState([]);
+
+//   const [searchParams] = useSearchParams();
+//   const history = useNavigate();
+
+//   useEffect(() => {
+//     const searchString = searchParams.get('query');
+//     if (searchString) {
+//       const getMovies = async () => {
+//         const { results } = await searchMovies(searchString);
+
+//         setMovies(results);
+//         setSearchMovie(searchString);
+//       };
+
+//       getMovies();
+//     }
+//   }, [searchParams]);
+
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+
+//     if (searchMovie.trim()) {
+//       const { results } = await searchMovies(searchMovie);
+
+//       setMovies(results);
+//       setSearchMovie('');
+
+//       if (results.length === 0) {
+//         window.alert('Sorry, we do not have such movie');
+//       }
+
+//       history({ search: `query=${searchMovie}` });
+//     }
+//   };
+
+//   return (
+//     <>
+//       <header>
+//         <SearchForm
+//           handleSubmit={handleSubmit}
+//           searchMovie={searchMovie}
+//           setSearchMovie={setSearchMovie}
+//         />
+//       </header>
+//       <MovieList movies={movies} />
+//     </>
+//   );
+// }
 
 // import React, { useState, useEffect } from 'react';
 // import { useLocation, useNavigate } from 'react-router-dom';
